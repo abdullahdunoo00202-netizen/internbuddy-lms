cfeca7823abbd6ca9ec51017253a030cd5e3c503
abdullahdunoo00202-netizen <abdullahdunoo00202@gmail.com>
2026-04-22 15:48:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -38,5 +38,11 @@ def lms_login():
 
     return redirect("/lms/dashboard")
 
+
+# 🔥 RUN SERVER (PRODUCTION SAFE)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=port)
